7348b380be40dca3956e22d8278b2d2b2f1912d9
NguyenQuangTuan <quangtuan0701@gmail.com>
2018-04-19 11:23:52 +0700

    dang nhap

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import DangNhap from './screens/DangNhap'
+export default class App extends Component{
+  render(){
+    return(
+        <DangNhap/>
+    )
+  }
+}
+
+// AppRegistry.registerComponent('Slack test', ()=> App)
